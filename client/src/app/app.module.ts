5b424a0b88d82529d1fe78037d9e84b1571afcf7
Noa Shay <noa224011224011@gmail.com>
2021-11-21 23:13:13 +0200

    side bar and search html css implementation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { CartComponent } from './cart/cart.component';
+import { CymptomIconComponent } from './cymptom-icon/cymptom-icon.component';
+import { CartIconComponent } from './cart-icon/cart-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutocompleteComponent,
+    SideBarComponent,
+    CartComponent,
+    CymptomIconComponent,
+    CartIconComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/cymptom-icon/cymptom-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cymptom-icon',
+  templateUrl: './cymptom-icon.component.html',
+  styleUrls: ['./cymptom-icon.component.css']
+})
+export class CymptomIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
