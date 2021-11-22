5886c3ce8bde3c7d7e81bc74bc85fcfb454eaf78
Noa Shay <noa224011224011@gmail.com>
2021-11-22 18:53:08 +0200

    get product by name route

---
+++ backend/api/controllers/products.js
@@ -0,0 +1,33 @@
+// const data = require("../../assets/products.json");
+const data = require("../../assets/bla.json");
+
+module.exports = {
+  getProduct: (req, res) => {
+    const productName = req.params.productName;
+
+    const finalProductArray = data.filter(
+      (product) => product.name === productName
+    );
+
+    console.log("productName:", productName);
+    console.log("finalProductArray:", finalProductArray);
+
+    if (finalProductArray.length === 0) {
+      res.status(404).json({
+        message: "NOT FOUND",
+      });
+    } else {
+      const finalProduct = finalProductArray[0];
+
+      const formatProduct = {
+        name: finalProduct.name,
+        image: finalProduct.image,
+        price: finalProduct.price,
+      };
+
+      res.status(200).json({
+        product: formatProduct,
+      });
+    }
+  },
+};
--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ var cors = require("cors");
 const app = express();
 const morgan = require("morgan");
 const autocompleteRoutes = require("./api/routes/autocomplete");
+const productsRoutes = require("./api/routes/products");
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/autocomplete", autocompleteRoutes);
+app.use("/api/getProduct", productsRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
