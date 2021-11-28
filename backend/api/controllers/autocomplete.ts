0134289bba4d7cd0cd990b85690bd1032734b578
Noa Shay <noa224011224011@gmail.com>
2021-11-28 14:53:30 +0200

    replace regex with "includes" function

--- backend/api/controllers/autocomplete.ts
+++ backend/api/controllers/autocomplete.ts
@@ -3,19 +3,15 @@ import { data } from "./data";
 
 export = {
   getSearchResults: (req: any, res: any) => {
-    const searchQuery = req.body.searchQuery;
+    const searchQuery = req.body.searchQuery.toLocaleLowerCase();
     if (searchQuery === "") {
       res.status(200).json([]);
     }
 
-    const regex = new RegExp(searchQuery, "gi");
-
     const matchingData = data
       .map((product: IProduct) => product.name)
       .filter((name: string) => name !== null)
-      .filter((name: string) => {
-        return name.match(regex);
-      });
+      .filter((name: string) => name.includes(searchQuery));
 
     const searchResults = matchingData.slice(0, 20);
     res.status(200).json({ searchQuery: searchResults });
